d52e54efdb1d9fb511cefbbd7dcec348492d054f
jm <johnmichaelmaytona@gmail.com>
2018-02-07 12:30:20 -0800

    Tabbed Content

--- app.js
+++ app.js
@@ -192,3 +192,39 @@ searchBar.addEventListener('keyup', function(e){
         }    
     })
 })
+
+
+//Tabbed Content
+
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+
+tabs.addEventListener('click', function(e){
+    if(e.target.tagName == "LI"){
+        const targetPanel = document.querySelector(e.target.dataset.owndata);
+        
+        panels.forEach(function(panel){
+            if(panel == targetPanel){
+                panel.classList.add('active');
+            }
+            else {
+                panel.classList.remove('active');
+            }
+        })
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
