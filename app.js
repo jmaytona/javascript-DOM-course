f0cce0f0f3aaa90493b430dfd342b6ad1cb0691b
jm <johnmichaelmaytona@gmail.com>
2018-02-07 10:21:35 -0800

    Search Bar function Added

--- app.js
+++ app.js
@@ -173,3 +173,22 @@ hideBox.addEventListener('change', function (e) {
     }
 
 });
+
+//Filter Search
+
+
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+
+    Array.from(books).forEach(function(book){
+    const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term) != -1){
+            book.style.display = 'block';
+        }
+        else {
+            book.style.display = 'none';
+        }    
+    })
+})
